b9c8a778f55870ac55ae1fb79df447dbe8afca54
Mai Lan Nguyen <mai.nguyen@hm.edu>
2025-02-10 09:30:33 +0100

    explanation

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./nav/Navigation";
 import Home from "./pages/Home";
 import AboutUs from "./pages/aboutus/AboutUs";
+import OurTeam from "./pages/aboutus/OurTeam";
 import Footer from "./footer/Footer";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/aboutus/ourteam" element={<OurTeam />} />
         </Routes>
         <Footer />
       </Router>
---
+++ app/src/pages/aboutus/OurTeam.js
@@ -0,0 +1,5 @@
+function OurTeam() {
+
+}
+
+export default OurTeam;
