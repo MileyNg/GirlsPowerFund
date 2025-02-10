c8f8f0d14bbe2e95781c90dcad26d4215f9ca6c2
hm-abdykaly <zhibek.abdykalykova@hm.edu>
2025-02-10 14:15:05 +0100

    add timeline

--- app/src/pages/aboutus/AboutUs.js
+++ app/src/pages/aboutus/AboutUs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "../../components/languages";
 import { useTranslation } from "react-i18next";
 import {
@@ -12,68 +13,75 @@ function AboutUs() {
   const { t } = useTranslation();
   return (
     <div>
-      <VerticalTimeline className="background-purple">
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentArrowStyle={{ borderRight: "7px solid" }}
-          date={<h1>2024</h1>}
-          iconStyle={{ background: "#49347C", color: "#fff" }}
-          icon={<SlArrowUp />}
-        >
-          <h2>{t("aboutus1title")}</h2>
-          <p>{t("aboutus1")}</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentArrowStyle={{ borderRight: "7px solid" }}
-          date={<h1>2023</h1>}
-          iconStyle={{ background: "#49347C", color: "#fff" }}
-          icon={<SlArrowUp />}
-        >
-          <h2>{t("aboutus2title")}</h2>
-          <p>{t("aboutus2")}</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentArrowStyle={{ borderRight: "7px solid" }}
-          date={<h1>2022</h1>}
-          iconStyle={{ background: "#49347C", color: "#fff" }}
-          icon={<SlArrowUp />}
-        >
-          <h2>{t("aboutus3title")}</h2>
-          <p>{t("aboutus3")}</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentArrowStyle={{ borderRight: "7px solid" }}
-          date={<h1>2021</h1>}
-          iconStyle={{ background: "#49347C", color: "#fff" }}
-          icon={<SlArrowUp />}
-        >
-          <h2>{t("aboutus4title")}</h2>
-          <p>{t("aboutus4")}</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          contentArrowStyle={{ borderRight: "7px solid" }}
-          date={<h1>2020</h1>}
-          iconStyle={{ background: "#49347C", color: "#fff" }}
-          icon={<SlArrowUp />}
-        >
-          <h2>{t("aboutus5title")}</h2>
-          <p>{t("aboutus5")}</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          contentArrowStyle={{ borderRight: "7px solid" }}
-          date={<h1>2019</h1>}
-          iconStyle={{ background: "#49347C", color: "#fff" }}
-          icon={<SlArrowUp />}
-        >
-          <h2>{t("aboutus6title")}</h2>
-          <p>{t("aboutus6")}</p>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
+      <Container className="background-purple">
+        <Row>
+          <div className="container quote">
+            <h1>{t("history")}</h1>
+          </div>
+          <VerticalTimeline>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentArrowStyle={{ borderRight: "7px solid" }}
+              date={<h1>2024</h1>}
+              iconStyle={{ background: "#49347C", color: "#fff" }}
+              icon={<SlArrowUp />}
+            >
+              <h2>{t("aboutus1title")}</h2>
+              <p>{t("aboutus1")}</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentArrowStyle={{ borderRight: "7px solid" }}
+              date={<h1>2023</h1>}
+              iconStyle={{ background: "#49347C", color: "#fff" }}
+              icon={<SlArrowUp />}
+            >
+              <h2>{t("aboutus2title")}</h2>
+              <p>{t("aboutus2")}</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentArrowStyle={{ borderRight: "7px solid" }}
+              date={<h1>2022</h1>}
+              iconStyle={{ background: "#49347C", color: "#fff" }}
+              icon={<SlArrowUp />}
+            >
+              <h2>{t("aboutus3title")}</h2>
+              <p>{t("aboutus3")}</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentArrowStyle={{ borderRight: "7px solid" }}
+              date={<h1>2021</h1>}
+              iconStyle={{ background: "#49347C", color: "#fff" }}
+              icon={<SlArrowUp />}
+            >
+              <h2>{t("aboutus4title")}</h2>
+              <p>{t("aboutus4")}</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--education"
+              contentArrowStyle={{ borderRight: "7px solid" }}
+              date={<h1>2020</h1>}
+              iconStyle={{ background: "#49347C", color: "#fff" }}
+              icon={<SlArrowUp />}
+            >
+              <h2>{t("aboutus5title")}</h2>
+              <p>{t("aboutus5")}</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--education"
+              contentArrowStyle={{ borderRight: "7px solid" }}
+              date={<h1>2019</h1>}
+              iconStyle={{ background: "#49347C", color: "#fff" }}
+              icon={<SlArrowUp />}
+            >
+              <h2>{t("aboutus6title")}</h2>
+              <p>{t("aboutus6")}</p>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        </Row>
+      </Container>
     </div>
   );
 }
